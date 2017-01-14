427519fdeb6434e982a1295d93a49b2ddaa9ac85
chenpeiyuan1987 <chenpeiyuan1987@aliyun.com>
2017-01-14 11:06:49 +0800

    read resource in test-classes or classes.

--- src/test/java/org/yuan/vita/demo/MainTest.java
+++ src/test/java/org/yuan/vita/demo/MainTest.java
@@ -1,12 +1,19 @@
 package org.yuan.vita.demo;
 
 import org.junit.Test;
+import java.net.URL;
 
 public class MainTest {
   
   @Test
   public void testMain() {
     Main.main(new String[0]);
+  }
+  
+  @Test
+  public void testResource() throws Exception {
+    URL url = ClassLoader.getSystemResource("spring.xml");
+    System.out.println(url.toURI().getPath());
   }
 
 }
